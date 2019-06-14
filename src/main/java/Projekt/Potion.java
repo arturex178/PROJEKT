270030431bbc8d7a66e96ec839b6abb1c8ccc57a
Strike2311 <49807482+Strike2311@users.noreply.github.com>
2019-06-14 23:24:17 +0200

    Commit#5

--- src/main/java/Projekt/Potion.java
+++ src/main/java/Projekt/Potion.java
@@ -7,4 +7,9 @@ public class Potion extends AProperties
         this.setHP(20);
     }
 
+    void heal(Witcher witcher){
+        if(witcher.getHP()<100 && witcher.getNumberOfPotions()>0){
+            witcher.setHP(witcher.getHP() + this.getHP());
+        }
+    }
 }
--- src/main/java/Projekt/Witcher.java
+++ src/main/java/Projekt/Witcher.java
@@ -1,6 +1,8 @@
 package Projekt;
-
+import java.util.Random;
 public class Witcher extends AUnit{
+
+    private int numberOfPotions = 0;
 
     public Witcher()
     {
@@ -9,17 +11,26 @@ public class Witcher extends AUnit{
         this.setDmgUp(12);
     }
 
+    private Random rnd = new Random();
+    public int getNumberOfPotions(){
+        return this.numberOfPotions;
+    }
 
+    public void setNumberOfPotions(int numberOfPotions){
+        this.numberOfPotions = numberOfPotions;
+    }
 
     public void fight(AUnit enemy){
 
         while(this.getHP() > 0 && enemy.getHP() > 0) {
+            this.special();
+            enemy.special();
             enemy.setHP(enemy.getHP() - this.decideDmg());
             this.setHP(this.getHP() - enemy.decideDmg());
-            this.special();
-            enemy.special();;
         }
-
+//        if(enemy.getHP() == 0) {
+  //          enemy.ifAlive() = false;
+   //     } else ifAlive() = false;
     }
 
     @Override
@@ -30,9 +41,57 @@ public class Witcher extends AUnit{
 
     public void move()
     {
+        switch (rnd.nextInt(4)){
+
+        case 0:
+            if(getPositionY() != 19){
+                setPositionY(getPositionY() + 1);
+            }
+            else setPositionY(0);
+            break;
+
+        case 1:
+            if(getPositionX() != 19){
+                setPositionX(getPositionX() + 1);
+            }
+            else setPositionX(0);
+            break;
+
+        case 2:
+            if(getPositionY() != 0){
+                setPositionY(getPositionY() - 1);
+            }
+            else setPositionY(19);
+            break;
+        case 3:
+            if(getPositionX() != 0){
+                setPositionX(getPositionX() - 1);
+            }
+            else setPositionX(19);
+        }
+    }
+
+    /*public void interact(AUnit object, Map map){
+
+        for(int i = 0; i < 400; i++){
+            if(map.field[i].getPositionX() == this.getPositionX() && map.field[i].getPositionY() == this.getPositionY())
+            {
+                if(map.field[i] instanceof AUnit){
+                this.fight(map.field[i]);
+                }
+                if(map.field[i] instanceof Potion) {
+                    this.setNumberOfPotions(getNumberOfPotions() + 1);
+                }
+                if(map.field[i] instanceof )
+
+            }
+            }
+
+        }
+            */
 
     }
 
 
-}
+
 
